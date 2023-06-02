5c79300d8cc51ef35e91525d3620ca6706fc191e
ftoprek-etfos <105854305+ftoprek-etfos@users.noreply.github.com>
2023-06-02 15:30:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 st.set_page_config(page_title="Predikcija spola i dobi")
 st.set_option('deprecation.showfileUploaderEncoding', False)
 c1, c2= st.columns(2)
+option = st.selectbox('Odaberite model?',('Model_1', 'Model_2'))
 def extract_face(image, required_size=(128, 128)):
   
   pixels = np.array(image.convert('RGB'))
@@ -42,14 +43,16 @@ def test_image(image,Model):
   img=img/255.0;
   pred_1=Model.predict(np.array([img]))
   
-  #age_dict = {0:'1-2', 1:'3-9',2:'10-20',3:'21-27',4:'28-45',5:'46-65',6:'65+'}
+  age_dict = {0:'1-2', 1:'3-9',2:'10-20',3:'21-27',4:'28-45',5:'46-65',6:'65+'}
   gender_dic=['Muško','Žensko']
-
+  
   age=(np.round(pred_1[1][0]))
   gender=int(np.round(pred_1[0][0]))
   c2.header('Izlaz')
-  #c2.subheader('Predviđena dob: '+age_dict[age])
-  c2.subheader('Predviđena dob: '+str(age[0]))
+  if option == 'Model_1':
+    c2.subheader('Predviđena dob: '+age_dict[age])
+  else:
+    c2.subheader('Predviđena dob: '+str(age[0]))
   c2.subheader('Predviđen spol: '+ gender_dic[gender])
   c2.image(image)
   c2.write(img.shape)
@@ -73,8 +76,10 @@ def main():
     c1.header('Ulaz')
     c1.image(im)
     c1.write(img.shape)
-
-    model = tf.keras.models.load_model("model_recg.h5")
+    if option == 'Model_1':
+      model = tf.keras.models.load_model("model_recg_1.h5")
+    else:
+      model = tf.keras.models.load_model("model_recg_2.h5")
     pixels = extract_face(im)
     test_image(pixels,model)
 if __name__ == '__main__':
